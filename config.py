8057d706ee430a778962daae6ca184653afa919f
Ihor <igor.l@myualab.com>
2023-05-17 23:21:43 +0300

    create balancing algoritm

---
+++ config.py
@@ -0,0 +1,71 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    MIN_DISBALANCE = float(getenv('MIN_DISBALANCE'))
+    LEVERAGE = float(getenv('LEVERAGE'))
+    TIMEOUT = float(getenv('TIMEOUT', 180))
+    ENV = getenv('ENV')
+    TELEGRAM_CHAT_ID = getenv('TELEGRAM_CHAT_ID')
+    TELEGRAM_TOKEN = getenv('TELEGRAM_TOKEN')
+
+    RABBIT = {
+        "host": getenv("RABBIT_HOST"),
+        "port": getenv("RABBIT_PORT", 5672),
+        "username": getenv("RABBIT_USERNAME"),
+        "password": getenv("RABBIT_PASSWORD")
+    }
+
+    BITMEX = {
+        "api_key": getenv("BITMEX_API_KEY"),
+        "api_secret": getenv("BITMEX_API_SECRET"),
+        "symbol": getenv("BITMEX_SYMBOL"),
+        "bitmex_shift": int(getenv("BITMEX_SHIFT"))
+    }
+
+    DYDX = {
+        "eth_address": getenv("DYDX_ETH_ADDRESS"),
+        "privateKey": getenv("DYDX_PRIVATE_KEY"),
+        "publicKeyYCoordinate": getenv("DYDX_PUBLIC_KEY_Y_COORDINATE"),
+        "publicKey": getenv("DYDX_PUBLIC_KEY"),
+        "eth_private_key": getenv("DYDX_ETH_PRIVATE_KEY"),
+        "infura_key": getenv("DYDX_INFURA_KEY"),
+        "key": getenv("DYDX_KEY"),
+        "secret": getenv("DYDX_SECRET"),
+        "passphrase": getenv("DYDX_PASSPHRASE"),
+        "symbol": getenv("DYDX_SYMBOL"),
+        "dydx_shift": int(getenv("DYDX_SHIFT"))
+    }
+
+    BINANCE = {
+        "api_key": getenv("BINANCE_API_KEY"),
+        "secret_key": getenv("BINANCE_SECRET_API"),
+        "symbol": getenv("BINANCE_SYMBOL"),
+        "binance_shift": int(getenv("BINANCE_SHIFT"))
+    }
+
+    OKX = {
+        "public_key": getenv("OKEX_PUBLIC_KEY"),
+        "secret_key": getenv("OKEX_SECRET_KEY"),
+        "passphrase": getenv("OKEX_PASSPHRASE"),
+        "symbol": getenv("OKEX_SYMBOL"),
+        "okex_shift": int(getenv("OKEX_SHIFT"))
+    }
+
+    KRAKEN = {
+        "api_key": getenv("KRAKEN_API_KEY"),
+        "secret_key": getenv("KRAKEN_SECRET_API"),
+        "symbol": getenv("KRAKEN_SYMBOL"),
+        "apollox_shift": int(getenv("KRAKEN_SHIFT"))
+    }
+
+    APOLLOX = {
+        "api_key": getenv("APOLLOX_API_KEY"),
+        "secret_key": getenv("APOLLOX_SECRET_API"),
+        "symbol": getenv("APOLLOX_SYMBOL"),
+        "apollox_shift": int(getenv("APOLLOX_SHIFT"))
+    }
---
+++ main.py
@@ -0,0 +1,115 @@
+import asyncio
+import time
+from pprint import pprint
+
+import aiohttp
+from aio_pika import connect_robust
+
+from clients.apollox import ApolloxClient
+from clients.binance import BinanceClient
+from clients.bitmex import BitmexClient
+from clients.dydx import DydxClient
+from clients.kraken import KrakenClient
+from clients.okx import OkxClient
+from config import Config
+from core.enums import PositionSideEnum
+
+
+class Balancing:
+    __slots__ = 'clients', 'positions', 'total_position', 'disbalance_coin', 'disbalance_usd', 'side', 'mq', 'session'
+
+    def __init__(self):
+        self.mq = None
+        self.clients = [
+            # BitmexClient(Config.BITMEX, Config.LEVERAGE),
+            DydxClient(Config.DYDX, Config.LEVERAGE),
+            BinanceClient(Config.BINANCE, Config.LEVERAGE),
+            # ApolloxClient(Config.APOLLOX, Config.LEVERAGE),
+            # OkxClient(Config.OKX, Config.LEVERAGE),
+            # KrakenClient(Config.KRAKEN, Config.LEVERAGE)
+        ]
+
+        self.__set_default()
+
+        for client in self.clients:
+            client.run_updater()
+
+        time.sleep(15)
+
+    async def run(self, loop) -> None:
+        print('START BALANCING')
+        async with aiohttp.ClientSession() as session:
+            await self.__setup_mq(loop)
+
+            while True:
+                await self.__get_positions()
+                await self.__check_disbalance()
+                await self.__balancing_positions(session)
+
+                self.__set_default()
+
+                time.sleep(Config.TIMEOUT)
+
+    async def __setup_mq(self, loop) -> None:
+        self.mq = await connect_robust(
+            f"amqp://{Config.RABBIT['username']}:{Config.RABBIT['password']}@{Config.RABBIT['host']}:{Config.RABBIT['port']}/",
+            loop=loop)
+        print('SETUP MQ')
+
+    def __set_default(self) -> None:
+        self.positions = {}
+        self.total_position = 0
+        self.disbalance_coin = 0
+        self.disbalance_usd = 0
+        self.side = 'LONG'
+
+    async def __get_positions(self) -> None:
+        for client in self.clients:
+            self.positions[client.EXCHANGE_NAME] = client.get_positions().get(client.symbol, {})
+
+        pprint(self.positions)
+
+    async def __check_disbalance(self) -> None:
+        long_coin = []
+        short_coin = []
+        long_usd = []
+        short_usd = []
+
+        for ecx_name, position in self.positions.items():
+            if position and position.get('side') == PositionSideEnum.LONG:
+                long_coin.append(position['amount'])
+                long_usd.append(position['amount_usd'])
+            elif position and position.get('side') == PositionSideEnum.SHORT:
+                short_coin.append(position['amount'])
+                short_usd.append(position['amount_usd'])
+
+        self.disbalance_coin = abs(sum([sum(long_coin), sum(short_coin)]))
+        self.disbalance_usd = abs(sum([sum(long_usd), sum(short_usd)]))
+        self.side = 'sell' if len(long_usd) > len(short_usd) else 'buy'
+
+        print(f'{self.disbalance_coin=}')
+        print(f'{self.disbalance_usd=}')
+        print(f'{self.side=}')
+
+    async def __balancing_positions(self, session) -> None:
+        print('START CHECK AND BALANCING')
+        tasks = []
+
+        if self.disbalance_usd > Config.MIN_DISBALANCE:
+            for client in self.clients:
+                price = client.get_orderbook().get(client.symbol, {}).get('bids' if self.side == 'LONG' else 'asks')[0][0] # noqa
+                tasks.append(client.create_order(
+                    amount=self.disbalance_coin / len(self.clients),
+                    side=self.side,
+                    price=price,
+                    session=session
+                ))
+
+        for res in await asyncio.gather(*tasks, return_exceptions=True):
+            print(res)
+
+
+if __name__ == '__main__':
+    worker = Balancing()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(worker.run(loop))
