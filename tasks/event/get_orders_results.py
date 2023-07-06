4653dbef424ae99735666e88b8375cbdfdd4daa0
Ihor <igor.l@myualab.com>
2023-07-06 11:49:35 +0300

    fixes

--- tasks/event/get_orders_results.py
+++ tasks/event/get_orders_results.py
@@ -17,14 +17,15 @@ class GetOrdersResults(BaseTask):
         for data in payload:
             try:
                 async with aiohttp.ClientSession() as session:
-                    res = await self.clients[data['exchange']].get_order_by_id(data['symbol'], data['order_ids'], session)
-                    print(f'{res=}')
 
-                    await self.publish_message(connect=self.app['mq'],
-                                               message=res,
-                                               routing_key=RabbitMqQueues.UPDATE_ORDERS,
-                                               exchange_name=RabbitMqQueues.get_exchange_name(RabbitMqQueues.UPDATE_ORDERS),
-                                               queue_name=RabbitMqQueues.UPDATE_ORDERS)
+
+                    if res := await self.clients[data['exchange']].get_order_by_id(data['symbol'], data['order_ids'], session):
+                        print(f'{res=}')
+                        await self.publish_message(connect=self.app['mq'],
+                                                   message=res,
+                                                   routing_key=RabbitMqQueues.UPDATE_ORDERS,
+                                                   exchange_name=RabbitMqQueues.get_exchange_name(RabbitMqQueues.UPDATE_ORDERS),
+                                                   queue_name=RabbitMqQueues.UPDATE_ORDERS)
 
             except (aiohttp.ServerDisconnectedError, ConnectionResetError):
                traceback.print_exc()
